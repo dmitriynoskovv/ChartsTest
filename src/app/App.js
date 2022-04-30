b22ed1c1cc18093a0cbedf25031616aec8f4ed8d
Klick99 <dmitriynoskov1994@gmail.com>
2022-04-30 14:52:01 +0300

    initial commit

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, Grid } from '@mui/material';
+
+const App = () => {
+  return (
+    <Grid container>
+      <Button variant="contained">Hello World</Button>
+    </Grid>
+  );
+};
+
+export { App };
